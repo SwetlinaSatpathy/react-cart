59328912247a1b445b9b14141d1a6b35b7288a1c
swetlina satpathy <swetlinasatpathy@swetlinas-MacBook-Pro.local>
2020-02-01 10:51:28 +0530

    Updates

---
+++ precache-manifest.d5f1aee898b0fa554a8f908b0fcfb20a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2d8d20623a36a82e02637a4ac43620",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "6bfd1a240596f6f2eb6b",
+    "url": "/react-cart/static/css/main.4bc451c5.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react-cart/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "6bfd1a240596f6f2eb6b",
+    "url": "/react-cart/static/js/main.e703aa45.chunk.js"
+  },
+  {
+    "revision": "d4d46f44ad693854d148",
+    "url": "/react-cart/static/js/runtime~main.641e3367.js"
+  }
+]);
